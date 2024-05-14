52375fcfdcdecee8fe93e8d1dd54332906867f24
Jassocc <clnearle@gmail.com>
2024-05-14 09:09:42 +0000

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+script for task 0
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    conbfiguratio nclass for html
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def indexing():
+    """
+    index route and func
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    determines best locale with match
+    """
+    res = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return res
+
+
+if __name__ == '__main__':
+    app.run()
