2294a1b5c273c702aed613c04dc9de23720d5466
Jassocc <clnearle@gmail.com>
2024-06-10 15:28:01 +0000

    task 7

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,13 @@
+// processes job
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
